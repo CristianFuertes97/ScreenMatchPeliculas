fd66fa00183522ecf4f67407f0ce26801b438c3c
CristianFuertes97 <cristianfuertesguerrero@gmail.com>
2024-12-05 11:43:18 -0500

    Version 1.0

---
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -0,0 +1,24 @@
+package com.aluracursos.screenmatch.repository;
+
+import com.aluracursos.screenmatch.dto.EpisodioDTO;
+import com.aluracursos.screenmatch.model.Categoria;
+import com.aluracursos.screenmatch.model.Episodio;
+import com.aluracursos.screenmatch.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SerieRepository extends JpaRepository<Serie,Long> {
+
+    Optional<Serie> findByTituloContainingIgnoreCase(String nombreSerie);
+    List<Serie> findTop5ByOrderByEvaluacionDesc();
+    List<Serie> findByGenero(Categoria categoria);
+
+    @Query("SELECT s FROM Serie s " + "JOIN s.episodios e " + "GROUP BY s " + "ORDER BY MAX(e.fechaDeLanzamiento) DESC LIMIT 5")
+    List<Serie> lanzamientosMasRecientes();
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s.id = :id AND e.temporada = :numeroTemporada")
+    List<Episodio> obtenerTemporadasPorNumero(Long id, Long numeroTemporada);
+}
